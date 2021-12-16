7a99c1c8df852ba9b619f0945bab96bf916e7bca
Danilocg07 <danilo-c-g@hotmail.com>
2021-12-16 15:51:08 -0300

    criacao do projeto

---
+++ src/main/java/plazzi/plazziadvocacia/PlazziAdvocaciaApplication.java
@@ -0,0 +1,13 @@
+package plazzi.plazziadvocacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazziAdvocaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazziAdvocaciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/plazzi/plazziadvocacia/PlazziAdvocaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package plazzi.plazziadvocacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlazziAdvocaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
